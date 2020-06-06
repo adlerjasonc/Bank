55aab4ed342940c8816ddb7ce3aadbc699744b0a
Jason Adler <jcadler@g.cofc.edu>
2020-06-06 19:48:11 -0400

    committed

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -11,8 +11,8 @@ namespace BankTests
         {
             // Arrange
             double beginningBalance = 12.00;
-            double debitAmount = 5.00;
-            double expected = 7.00;
+            double debitAmount = 6.00;
+            double expected = 6.00;
             BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
 
             // Act
